12d23f3e1c08865966b8d53874bc6c73f56ccdac
esaumorales <esaumorales2322@gmail.com>
2025-06-15 23:55:22 -0500

    fixed header

--- src/presentation/shared/components/header.jsx
+++ src/presentation/shared/components/header.jsx
@@ -1,21 +1,28 @@
 import LOGO from '@/assets/image/logo.webp'
-
 import '@/presentation/styles/common/header.css'
 import { Link } from 'react-router-dom'
 
 export default function Header() {
-
     const List = [
-
         {
             id: 1,
             name: 'Medicos',
-            link: '/medicos'
+            link: '/medicos',
+            dropdown: [
+                { id: 1, name: 'Listado de medicos', link: '/listademedicos' },
+                { id: 2, name: 'Añadir medicos', link: '/anadirmedicos' },
+                { id: 3, name: 'Especialidades', link: '/especialidades' }
+            ]
         },
         {
             id: 2,
             name: 'Pacientes',
-            link: '/pacientes'
+            link: '/pacientes',
+            dropdown: [
+                { id: 1, name: 'Listado de pacientes', link: '/listadopacientes' },
+                { id: 2, name: 'Añadir pacientes', link: '/anadirpacientes' },
+                { id: 3, name: 'Obras sociales', link: '/obrassociales' }
+            ]
         },
         {
             id: 3,
@@ -42,37 +49,51 @@ export default function Header() {
             name: 'Facturacion',
             link: '/facturacion'
         },
-    ]
+    ];
 
     return (
-        <div className='header'>
-            <div className='header__navbar'>
-                <div className='header__logo'>
+        <div className="header">
+            <div className="header__navbar">
+                <div className="header__logo">
                     <img src={LOGO} alt="logo__header" />
                 </div>
-                <div className='header__navbar--list'>
+                <div className="header__navbar--list">
                     <nav>
                         <ul>
                             {
-                                List.map((item) => (
-                                    <li key={item.id}>
-                                        <Link to={item.link} className='header__navbar--style' >{item.name}</Link>
-                                    </li>
-                                ))
+                                List.map((item) => {
+                                    return (
+                                        <li key={item.id} className="header__navbar--container">
+                                            <Link
+                                                to={item.link}
+                                                className="header__navbar--style"
+                                                aria-label={`Ir a ${item.name}`}
+                                            >
+                                                {item.name}
+                                            </Link>
+                                            {item.dropdown && (
+                                                <ul className="header__navbar--menu">
+                                                    {item.dropdown.map((subItem) => (
+                                                        <li key={subItem.id}>
+                                                            <Link to={subItem.link}>
+                                                                {subItem.name}
+                                                            </Link>
+                                                        </li>
+                                                    ))}
+                                                </ul>
+                                            )}
+                                        </li>
+                                    )
+                                })
                             }
                         </ul>
                     </nav>
                 </div>
             </div>
-            <div className='header__button'>
-                <button>
-                    <Link
-                        to='/login'
-                        className='header__button--style'
-                        >
+            <div className="header__button">
+                    <Link to='/login' className="header__button--style">
                         Iniciar Sesion
                     </Link>
-                </button>
             </div>
         </div>
     )
